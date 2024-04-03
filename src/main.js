b964e2ecfd9e8bfcdb0edeec5531d5dbf818aa94
leehj8896 <leehj8896@gmail.com>
2024-04-04 00:08:23 +0900

    firebase 추가

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from "firebase/firestore"
 
-createApp(App).mount('#app')
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
+}
+const firebaseApp = initializeApp(firebaseConfig)
+const db = getFirestore(firebaseApp)
+
+const app = createApp(App)
+app.config.globalProperties.$db = db
+app.mount('#app')
